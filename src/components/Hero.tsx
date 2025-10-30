3027c397ab33b473129b398937d9ad080b81b7ae
Matěj Štipčák <matej.stipcak@gmail.com>
2025-10-30 09:36:53 +0100

    Render animated hero only

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,29 +1,7 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-import { useEffect, useState } from 'react';
-
-import HeroStatic from './HeroStatic';
-import { useReducedMotion } from '@/hooks/useReducedMotion';
-
-const HeroAnimated = dynamic(() => import('./HeroAnimated'), {
-  ssr: false,
-  loading: () => <HeroStatic />,
-});
+import HeroAnimated from './HeroAnimated';
 
 export default function Hero() {
-  const prefersReducedMotion = useReducedMotion();
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    if (!prefersReducedMotion) {
-      setIsMounted(true);
-    }
-  }, [prefersReducedMotion]);
-
-  if (prefersReducedMotion || !isMounted) {
-    return <HeroStatic />;
-  }
-
   return <HeroAnimated />;
 }
