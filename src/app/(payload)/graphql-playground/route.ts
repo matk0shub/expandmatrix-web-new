df5196b2dbcb2ba2782f392c3cd92adde10023d8
Matěj Štipčák <matej.stipcak@gmail.com>
2025-10-31 17:27:23 +0100

    chore(payload): add (payload) route group (admin/api/graphql)

---
+++ src/app/(payload)/graphql-playground/route.ts
@@ -0,0 +1,8 @@
+/* THIS FILE WAS GENERATED BASED ON THE OFFICIAL PAYLOAD TEMPLATE. */
+import '@payloadcms/next/css';
+
+import config from '@payload-config';
+import { GRAPHQL_PLAYGROUND_GET, REST_OPTIONS } from '@payloadcms/next/routes';
+
+export const GET = GRAPHQL_PLAYGROUND_GET(config);
+export const OPTIONS = REST_OPTIONS(config);
