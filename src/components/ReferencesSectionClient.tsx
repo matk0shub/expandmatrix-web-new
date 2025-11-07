c8f2ba1b524465cc12d40e5159aa5fe3df73527e
Matěj Štipčák <matej.stipcak@gmail.com>
2025-11-07 17:58:40 +0100

    style(references): add inner padding to full-bleed card

--- src/components/ReferencesSectionClient.tsx
+++ src/components/ReferencesSectionClient.tsx
@@ -95,7 +95,8 @@ export default function ReferencesSectionClient({ references, copy }: References
       <meta itemProp="description" content={copy.metaDescription} />
 
       <div className="relative w-full mx-auto max-w-[1780px] px-6 sm:px-10 md:px-14 lg:px-16 xl:px-20 2xl:px-24">
-        <div className="relative w-full rounded-[40px] overflow-hidden bg-black/60 border border-white/10 shadow-[0_40px_120px_rgba(0,0,0,0.6)] aspect-[16/9] min-h-[420px]">
+        <div className="relative w-full h-full rounded-[48px] p-3 sm:p-4">
+          <div className="relative w-full h-full rounded-[40px] overflow-hidden bg-black/60 border border-white/10 shadow-[0_40px_120px_rgba(0,0,0,0.6)] aspect-[16/9] min-h-[420px]">
           <AnimatePresence mode="wait" initial={false}>
             {activeReference && (
               <ReferenceBackground
@@ -150,6 +151,7 @@ export default function ReferencesSectionClient({ references, copy }: References
               </AnimatePresence>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </section>
